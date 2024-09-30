731b84df7e340ac926a9d0377ce293e14a8d987a
Andreyenka Andrei <64132068+poslannikdyni@users.noreply.github.com>
2024-09-30 05:52:55 +0300

    1

--- src/main/java/by/clevertec/lib/ReflectionConvertor.java
+++ src/main/java/by/clevertec/lib/ReflectionConvertor.java
@@ -2,6 +2,7 @@ package by.clevertec.lib;
 
 import by.clevertec.lib.api.AJConvertorContext;
 import by.clevertec.lib.exceptions.AndreiJsonovichInternalException;
+import by.clevertec.lib.intermediate_representation.AJArray;
 import by.clevertec.lib.intermediate_representation.AJElement;
 import by.clevertec.lib.intermediate_representation.AJObject;
 import by.clevertec.lib.intermediate_representation.AJProperty;
@@ -17,9 +18,12 @@ import by.clevertec.lib.reflection.impl.StringConverter;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class ReflectionConvertor {
     static final Map<String, TypeConvertor> CONVERTORS;
@@ -70,10 +74,14 @@ public class ReflectionConvertor {
             if (Modifier.isStatic(modifiers) || Modifier.isTransient(modifiers)) continue;
 
             field.setAccessible(true);
-            var fieldType = field.getType().getName();
-            var convertor = CONVERTORS.get(fieldType);
+            var fieldType = field.getType();
+            var convertor = CONVERTORS.get(fieldType.getName());
 
-            if (convertor != null) {
+            if (List.class.isAssignableFrom(fieldType) || Set.class.isAssignableFrom(fieldType) || fieldType.isArray()) {
+                asArray(rez, field, object, context);
+            } else if (Map.class.isAssignableFrom(fieldType)) {
+                asMapArray(rez, field, object, context);
+            } else if (convertor != null) {
                 rez.addProperty(new AJProperty(field.getName(), convertor.toAJElement(field.get(object), context)));
             } else if (Object.class.isAssignableFrom(field.getType())) {
                 addProperty(rez, field.getName(), context.toAJElement(field.get(object)));
@@ -84,24 +92,107 @@ public class ReflectionConvertor {
         return rez;
     }
 
+    private void asArray(AJObject rez, Field field, Object object, AJConvertorContext context) throws IllegalAccessException {
+        Object[] items;
+        if (Collection.class.isAssignableFrom(field.getType())) {
+            items = ((Collection) field.get(object)).toArray();
+        } else if (field.getType().isArray()) {
+            items = (Object[]) field.get(object);
+        } else {
+            throw new RuntimeException("TODO"); //TODO
+        }
+
+        AJArray ajArray = new AJArray();
+        for (Object item : items) {
+            ajArray.add(context.toAJElement(item));
+        }
+
+        addProperty(rez, field.getName(), ajArray);
+    }
+
+    private <T> void asMapArray(AJObject rez, Field field, T object, AJConvertorContext context) throws IllegalAccessException {
+        Map<Object, Object> map = (Map<Object, Object>) field.get(object);
+        AJArray ajArray = new AJArray();
+        for (var item : map.entrySet()) {
+            var key = context.toAJElement(item.getKey());
+            var value = context.toAJElement(item.getValue());
+            AJArray itemArray = new AJArray();
+            itemArray.add(key);
+            itemArray.add(value);
+            ajArray.add(itemArray);
+        }
+
+        addProperty(rez, field.getName(), ajArray);
+    }
+
     public <T> T toUserType(Class<T> requestType, AJElement element, AJConvertorContext context) {
         try {
-            var deserializeClass = requestType.getName();
-            var standartConvertor = CONVERTORS.get(deserializeClass);
-            if (standartConvertor != null) {
-                return (T) standartConvertor.toUserType(element, context);
+            if (List.class.isAssignableFrom(requestType)) {
+                return toList(requestType, element, context);
+            }else if (Set.class.isAssignableFrom(requestType)) {
+                return toSet(requestType, element, context);
+            }else if (requestType.isArray()) {
+                return toArray(requestType, element, context);
+            } else if (Map.class.isAssignableFrom(requestType)) {
+                return toMap(requestType, element, context);
             } else {
-                return build(requestType, element, context);
+                return advanced(requestType, element, context);
             }
         } catch (Exception e) {
             throw new AndreiJsonovichInternalException(e);
         }
     }
 
-    private <T> T build(Class<T> requestType, AJElement element, AJConvertorContext context) {
-        if(element.getType() != AJElement.Type.OBJECT)
-            throw new AndreiJsonovichInternalException("Json convert failed");
+    private <T> T toList(Class<T> requestType, AJElement element, AJConvertorContext context) {
+        AJArray ajArray = expectedArray(element);
 
+//                var listWithGeneric = requestType.getGenericType();
+//                Class actualType = null;
+//                if (listWithGeneric instanceof ParameterizedType p) {
+//                    if (p.getActualTypeArguments().length < 1)
+//                        throw new AndreiJsonovichInternalException("Expected parameterized type");
+//                    actualType = Class.forName(p.getActualTypeArguments()[0].getTypeName());
+//                }
+//
+//                AJArray array = (AJArray) property.getValue();
+//                List<Object> list = new ArrayList<>();
+//                for (AJElement ajElement : array.getAll()) {
+//                    list.add(context.toUserType(actualType, (AJObject) ajElement));
+//                }
+//
+//                if (List.class.isAssignableFrom(field.getType()))
+//                    field.set(instance, list);
+//                else if (Set.class.isAssignableFrom(field.getType()))
+//                    field.set(instance, new HashSet<>(list));
+//                else if (field.getType().isArray())
+//                    field.set(instance, list.toArray());
+//                else throw new AndreiJsonovichInternalException("Unexpected behaviour");
+        return null;
+    }
+
+    private <T> T toSet(Class<T> requestType, AJElement element, AJConvertorContext context) {
+        return null;
+    }
+
+    private <T> T toArray(Class<T> requestType, AJElement element, AJConvertorContext context) {
+        return null;
+    }
+
+    private <T> T toMap(Class<T> requestType, AJElement element, AJConvertorContext context) {
+        return null;
+    }
+
+    private <T> T advanced(Class<T> requestType, AJElement element, AJConvertorContext context) {
+        var deserializeClass = requestType.getName();
+        var standartConvertor = CONVERTORS.get(deserializeClass);
+        if (standartConvertor != null) {
+            return (T) standartConvertor.toUserType(element, context);
+        } else {
+            return buildObject(requestType, element, context);
+        }
+    }
+
+    private <T> T buildObject(Class<T> requestType, AJElement element, AJConvertorContext context) {
         try {
             T instance = requestType.newInstance();
             var fields = requestType.getDeclaredFields();
@@ -111,11 +202,11 @@ public class ReflectionConvertor {
 
                 field.setAccessible(true);
                 var fieldType = field.getType().getName();
-                var property = ((AJObject)element).getProperty(field.getName());
+                var property = ((AJObject) element).getProperty(field.getName());
                 var convertor = CONVERTORS.get(fieldType);
 
                 if (convertor != null) {
-                    field.set(instance, convertor.toUserType(element, context));
+                    field.set(instance, convertor.toUserType(property.getValue(), context));
                 } else if (Object.class.isAssignableFrom(field.getType())) {
                     field.set(instance, context.toUserType(field.getType(), property));
                 } else {
@@ -130,33 +221,12 @@ public class ReflectionConvertor {
         }
     }
 
-    //    private <T> void asListOrSetOrArray(T instance, Field field, AJProperty property, AJConvertorContext context) throws ClassNotFoundException, IllegalAccessException {
-    //        if (property.getValue().getType() != AJElement.Type.ARRAY)
-    //            throw new AndreiJsonovichInternalException("Unexpected behaviour");
-    //
-    //        var listWithGeneric = field.getGenericType();
-    //        Class actualType = null;
-    //        if (listWithGeneric instanceof ParameterizedType p) {
-    //            if (p.getActualTypeArguments().length < 1)
-    //                throw new AndreiJsonovichInternalException("Expected parameterized type");
-    //            actualType = Class.forName(p.getActualTypeArguments()[0].getTypeName());
-    //        }
-    //
-    //        AJArray array = (AJArray) property.getValue();
-    //        List<Object> list = new ArrayList<>();
-    //        for (AJElement ajElement : array.getAll()) {
-    //            list.add(context.toUserType(actualType, (AJObject) ajElement));
-    //        }
-    //
-    //        if (List.class.isAssignableFrom(field.getType()))
-    //            field.set(instance, list);
-    //        else if (Set.class.isAssignableFrom(field.getType()))
-    //            field.set(instance, new HashSet<>(list));
-    //        else if (field.getType().isArray())
-    //            field.set(instance, list.toArray());
-    //        else throw new AndreiJsonovichInternalException("Unexpected behaviour");
-    //
-    //    }
+    private AJArray expectedArray(AJElement element) {
+        if (element.getType() != AJElement.Type.ARRAY)
+            throw new ClassCastException("Expected ARRAY, got " + element.getType() + System.lineSeparator() + element.toString());
+        return (AJArray) element;
+    }
+
     //    private <T> void asObject(AJObject target, Field field, T object, AJConvertorContext context) throws
     //            IllegalAccessException {
     //        addProperty(target, field.getName(), context.toAJObject(field.get(object)));
